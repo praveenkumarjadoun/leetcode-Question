9ba311628c8ab05e7960ffe650b92e4447a76c5b
Praveen Kumar Jadoun <thakurpraveen9634@gmail.com>
2026-07-19 22:35:55 +0530

    Time: 2 ms (6.76%) | Memory: 47.3 MB (5.23%) - LeetSync

--- 941-sort-array-by-parity/sort-array-by-parity.java
+++ 941-sort-array-by-parity/sort-array-by-parity.java
@@ -2,17 +2,20 @@ class Solution {
     public int[] sortArrayByParity(int[] nums) {
         ArrayList<Integer> list1 = new ArrayList<>();
         ArrayList<Integer> list2 = new ArrayList<>();
-        for(int i =0;i<nums.length;i++){
-            if(nums[i] % 2 == 0){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] % 2 == 0) {
                 list1.add(nums[i]);
-            }else{
+            } else {
                 list2.add(nums[i]);
             }
         }
-        list1.addAll(list2);
+        for (int x : list2) {
+            list1.add(x);
+        }
         int ans[] = new int[list1.size()];
-        for(int i =0;i<list1.size();i++){
+        for (int i = 0; i < list1.size(); i++) {
             ans[i] = list1.get(i);
-        }return ans;
+        }
+        return ans;
     }
 }
